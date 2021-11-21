0f9c38aa2fa71178d533f61d81c2eacef9a56501
ZhukAndrei <3663164@mail.ru>
2021-11-21 12:50:45 +0300

    begin home work #6.  Step 2.

--- 6/js/script.js
+++ 6/js/script.js
@@ -1,7 +1,11 @@
-const animals = [{ name: "", color: "", age: "", voice: 'Moo' }, { name: "", color: "", age: "", voice: 'Moo' }, { name: "", color: "", age: "", voice: 'Moo' }];
+const animals = [{ name: "Cat", color: "ginger", age: "", voice: 'meow' }, 
+{ name: "Dog", color: "black-white", age: "", voice: 'woof' }, 
+{ name: "Horse", color: "black", age: "", voice: 'neigh' },
+{ name: "Pig", color: "pink", age: "", voice: 'oink' },
+{ name: "Cow", color: "white", age: "", voice: 'moo' }];
 
 class Animal {
-  constructor(name, color, age, voice) {
+  constructor(name, color, age) {
       this.name = name;
       this.name = color;
       this.name = age;
@@ -9,12 +13,16 @@ class Animal {
   }
 
   say() {
-
-  }
-}
-
+return `Hello, I am ${this.name} and I am ${this.age} years old. My color ${this.color} and I speak ${this.voice}. ))`
+       }
+    }
 function animalsFabric(animals) {
-  //Что-то делаем с массивом
-  return objectAnimals
-
+    const newAnimals = animals.map((animals) =>
+    
+    ) ;
+return ObjectAnimals;
+    
+  //;
+  //return objectAnimals
 }
+alert(say());
