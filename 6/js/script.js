58df1be11800b789d90a9b643f36a61fd9564c2a
ZhukAndrei <3663164@mail.ru>
2021-11-21 11:54:48 +0300

    begin home work #6.  Step 1.

---
+++ 6/js/script.js
@@ -0,0 +1,20 @@
+const animals = [{ name: "", color: "", age: "", voice: 'Moo' }, { name: "", color: "", age: "", voice: 'Moo' }, { name: "", color: "", age: "", voice: 'Moo' }];
+
+class Animal {
+  constructor(name, color, age, voice) {
+      this.name = name;
+      this.name = color;
+      this.name = age;
+      this.name = voice;
+  }
+
+  say() {
+
+  }
+}
+
+function animalsFabric(animals) {
+  //Что-то делаем с массивом
+  return objectAnimals
+
+}
