a7830b81f60141a38ac50136a9220bbe74128254
ZhukAndrei <3663164@mail.ru>
2021-11-21 22:43:55 +0300

    begin home work #6.  Step 5.

--- 6/js/script.js
+++ 6/js/script.js
@@ -1,8 +1,8 @@
-const animals = [{ name: "", color: "", age: "2", voice: 'meow' }, 
-{ name: "Dog", color: "black-white", age: "7", voice: 'woof' }, 
-{ name: "Horse", color: "black", age: "6", voice: 'neigh' },
-{ name: "Pig", color: "pink", age: "1", voice: 'oink' },
-{ name: "Cow", color: "white", age: "3", voice: 'moo' }];
+const animals = [{ name: "", color: "", age: "", voice: '' }, 
+{ name: "", color: "", age: "", voice: '' }, 
+{ name: "", color: "", age: "", voice: '' },
+{ name: "", color: "", age: "", voice: '' },
+{ name: "", color: "", age: "", voice: '' }];
 
 class Animal {
   constructor(name, color, age, voice) {
@@ -11,22 +11,38 @@ class Animal {
       this.age = age;
       this.voice = voice;
         }
-    say(x){
-        return  this.name, this.color, this.age, this.voice;
+    nameAnimal(){
+        return  this.name;
             }
+    colorAnimal(){
+        return this.color;
     }
-    const animal1 = new Animal(("Cat"));
-    const animal2 = new Animal(("Dog"));
-    const animal3 = new Animal(("Pig"));
-    const animal4 = new Animal(("COW"));
+    ageAnimal(){
+        return this.age;
+        }
+    voiceAnimal() {
+        return this.voice;
+    }
+    }
+    const animal1 = new Animal("Cat", "ginger", "2", "meow");
+    const animal2 = new Animal(("Dog", "black-white", "7", "woof"));
+    const animal3 = new Animal(("Horse", "black", "6", 'neigh'));
+    const animal4 = new Animal(("Pig", "pink", "1", "oink"));
+    const animal5 = new Animal(("Cow", "white", "1", "moo"));
+    
+    console.log(animal1);
+    console.log(animal2);
+    console.log(animal3);
+    console.log(animal4);
+    console.log(animal5);
+    // 
+    // 
+    // 
 
-    //console.log(let objectAnimals = [animal1, animal2, animal3, animal4]);
+    //console.log(animal1, animal2, animal3, animal4]
 
 
-            console.log(animal1);
-            console.log(animal2);
-            console.log(animal3);
-            console.log(animal4);
+//console.log(objectAnimals);
 
 
         // var newAnimal1 = array.map(function(x) {
