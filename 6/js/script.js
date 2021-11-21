ec9ba9ccc8f698d8c2f27757578b81db02eb25fa
ZhukAndrei <3663164@mail.ru>
2021-11-21 13:06:46 +0300

    begin home work #6.  Step 3.

--- 6/js/script.js
+++ 6/js/script.js
@@ -1,28 +1,29 @@
-const animals = [{ name: "Cat", color: "ginger", age: "", voice: 'meow' }, 
-{ name: "Dog", color: "black-white", age: "", voice: 'woof' }, 
-{ name: "Horse", color: "black", age: "", voice: 'neigh' },
-{ name: "Pig", color: "pink", age: "", voice: 'oink' },
-{ name: "Cow", color: "white", age: "", voice: 'moo' }];
+const animals = [{ name: "Cat", color: "ginger", age: "2", voice: 'meow' }, 
+{ name: "Dog", color: "black-white", age: "7", voice: 'woof' }, 
+{ name: "Horse", color: "black", age: "6", voice: 'neigh' },
+{ name: "Pig", color: "pink", age: "1", voice: 'oink' },
+{ name: "Cow", color: "white", age: "3", voice: 'moo' }];
 
 class Animal {
   constructor(name, color, age) {
       this.name = name;
-      this.name = color;
-      this.name = age;
-      this.name = voice;
+      this.color = color;
+      this.age = age;
+      this.voice = voice;
   }
 
   say() {
-return `Hello, I am ${this.name} and I am ${this.age} years old. My color ${this.color} and I speak ${this.voice}. ))`
+      return this.voice;
+//return `Hello, I am ${this.name} and I am ${this.age} years old. My color ${this.color} and I speak ${this.voice}. ))`
        }
     }
 function animalsFabric(animals) {
-    const newAnimals = animals.map((animals) =>
+    const newAnimals = animals.map((say() => 
     
-    ) ;
+    );
 return ObjectAnimals;
     
   //;
   //return objectAnimals
 }
-alert(say());
+alert(say(Cow));
