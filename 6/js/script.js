0fb475980c445e56c556e872fd9870478cf7f09d
ZhukAndrei <3663164@mail.ru>
2021-11-21 22:58:43 +0300

    begin home work #6.  Step 6.

--- 6/js/script.js
+++ 6/js/script.js
@@ -1,70 +1,44 @@
-const animals = [{ name: "", color: "", age: "", voice: '' }, 
-{ name: "", color: "", age: "", voice: '' }, 
-{ name: "", color: "", age: "", voice: '' },
-{ name: "", color: "", age: "", voice: '' },
-{ name: "", color: "", age: "", voice: '' }];
+const animals = [{ name: "", color: "", age: "", voice: "" }];
 
 class Animal {
   constructor(name, color, age, voice) {
-      this.name = name;
-      this.color = color;
-      this.age = age;
-      this.voice = voice;
-        }
-    nameAnimal(){
-        return  this.name;
-            }
-    colorAnimal(){
-        return this.color;
-    }
-    ageAnimal(){
-        return this.age;
-        }
-    voiceAnimal() {
-        return this.voice;
-    }
-    }
-    const animal1 = new Animal("Cat", "ginger", "2", "meow");
-    const animal2 = new Animal(("Dog", "black-white", "7", "woof"));
-    const animal3 = new Animal(("Horse", "black", "6", 'neigh'));
-    const animal4 = new Animal(("Pig", "pink", "1", "oink"));
-    const animal5 = new Animal(("Cow", "white", "1", "moo"));
-    
-    console.log(animal1);
-    console.log(animal2);
-    console.log(animal3);
-    console.log(animal4);
-    console.log(animal5);
-    // 
-    // 
-    // 
+    this.name = name;
+    this.color = color;
+    this.age = age;
+    this.voice = voice;
+  }
+  /*nameAnimal() {
+    return this.name;
+  }
+  colorAnimal() {
+    return this.color;
+  }
+  ageAnimal() {
+    return this.age;
+  }
+  voiceAnimal() {
+    return this.voice;
+  }*/
+}
+const animal1 = new Animal("Cat", "ginger", "2", "meow");
+const animal2 = new Animal("Dog", "black-white", "7", "woof");
+const animal3 = new Animal("Horse", "black", "6", "neigh");
+const animal4 = new Animal("Pig", "pink", "1", "oink");
+const animal5 = new Animal("Cow", "white", "3", "moo");
 
-    //console.log(animal1, animal2, animal3, animal4]
+console.log(animal1);
+console.log(animal2);
+console.log(animal3);
+console.log(animal4);
+console.log(animal5);
 
 
-//console.log(objectAnimals);
 
+//function animalsFabric(animals) {
+//
+//}
 
-        // var newAnimal1 = array.map(function(x) {
-        //     return for(let i = 0; i <= array.length - 1; i++){
-            
-        //       }  alert(array[i]);;
-//   say(voice) {
-//       let voice2 = this.voice;
-//       console.log(voice2);
-//       return this.voice = voice;
-     
-    //     }
-        
-    // }
-    
+    //Что-то делаем с массивом
+    //return objectAnimals
+  
 
-     
-    // console.log(cat.Animal);
-
-
-    //function animalsFabric(animals) {
-        
-   // };
-        //Что-то делаем с массивом
-       //return objectAnimals
