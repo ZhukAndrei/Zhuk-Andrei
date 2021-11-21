83cc724d6ebbbb8b85810b83662f452124706f69
ZhukAndrei <3663164@mail.ru>
2021-11-21 22:10:23 +0300

    begin home work #6.  Step 4.

--- 6/js/script.js
+++ 6/js/script.js
@@ -1,29 +1,54 @@
-const animals = [{ name: "Cat", color: "ginger", age: "2", voice: 'meow' }, 
+const animals = [{ name: "", color: "", age: "2", voice: 'meow' }, 
 { name: "Dog", color: "black-white", age: "7", voice: 'woof' }, 
 { name: "Horse", color: "black", age: "6", voice: 'neigh' },
 { name: "Pig", color: "pink", age: "1", voice: 'oink' },
 { name: "Cow", color: "white", age: "3", voice: 'moo' }];
 
 class Animal {
-  constructor(name, color, age) {
+  constructor(name, color, age, voice) {
       this.name = name;
       this.color = color;
       this.age = age;
       this.voice = voice;
-  }
-
-  say() {
-      return this.voice;
-//return `Hello, I am ${this.name} and I am ${this.age} years old. My color ${this.color} and I speak ${this.voice}. ))`
-       }
+        }
+    say(x){
+        return  this.name, this.color, this.age, this.voice;
+            }
     }
-function animalsFabric(animals) {
-    const newAnimals = animals.map((say() => 
+    const animal1 = new Animal(("Cat"));
+    const animal2 = new Animal(("Dog"));
+    const animal3 = new Animal(("Pig"));
+    const animal4 = new Animal(("COW"));
+
+    //console.log(let objectAnimals = [animal1, animal2, animal3, animal4]);
+
+
+            console.log(animal1);
+            console.log(animal2);
+            console.log(animal3);
+            console.log(animal4);
+
+
+        // var newAnimal1 = array.map(function(x) {
+        //     return for(let i = 0; i <= array.length - 1; i++){
+            
+        //       }  alert(array[i]);;
+//   say(voice) {
+//       let voice2 = this.voice;
+//       console.log(voice2);
+//       return this.voice = voice;
+     
+    //     }
+        
+    // }
     
-    );
-return ObjectAnimals;
-    
-  //;
-  //return objectAnimals
-}
-alert(say(Cow));
+
+     
+    // console.log(cat.Animal);
+
+
+    //function animalsFabric(animals) {
+        
+   // };
+        //Что-то делаем с массивом
+       //return objectAnimals
