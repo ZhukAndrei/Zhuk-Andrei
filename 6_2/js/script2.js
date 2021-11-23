5ebf9b47fe10bff60dec353e6f8e1ac58100b36c
ZhukAndrei <93341242+ZhukAndrei@users.noreply.github.com>
2021-11-24 00:10:29 +0300

    begin home work #6. Step 10. Function method

---
+++ 6_2/js/script2.js
@@ -0,0 +1,28 @@
+const animals = [{ name: "", color: "", age: "", voice: "" }];
+
+class Animal {
+  constructor(name, color, age, voice) {
+    this.name = name;
+    this.color = color;
+    this.age = age;
+    this.voice = voice;
+  }
+}
+const animal1 = new Animal("Cat", "ginger", "2", "meow");
+const animal2 = new Animal("Dog", "black-white", "7", "woof");
+const animal3 = new Animal("Horse", "black", "6", "neigh");
+const animal4 = new Animal("Pig", "pink", "1", "oink");
+const animal5 = new Animal("Cow", "white", "3", "moo");
+
+objectAnimals = [];
+animals.map((animals) => {
+  key = true;
+  objectAnimals.push(animal1);
+  objectAnimals.push(animal2);
+  objectAnimals.push(animal3);
+  objectAnimals.push(animal4);
+  objectAnimals.push(animal5);
+});
+
+console.log(objectAnimals);
+//
