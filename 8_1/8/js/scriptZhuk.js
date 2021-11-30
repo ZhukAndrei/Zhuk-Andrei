227fd04704b908177a1fcb5f8607b54587c1da2f
ZhukAndrei <93341242+ZhukAndrei@users.noreply.github.com>
2021-11-30 16:29:46 +0300

    added 8 begin

---
+++ 8_1/8/js/scriptZhuk.js
@@ -0,0 +1,82 @@
+
+let header = document.createElement("header");
+    header.className = "header";
+    document.body.append(header);
+let nav = document.createElement("nav");
+    nav.className = "header_block";
+    nav.innerHTML = "COMPANY  LOGO";
+    header.appendChild(nav);
+
+
+// let a = document.createElement('a');
+//  a.innerHTML = "href='#'";
+//  li.appendChild(a);
+
+    
+let arr = ["Google", "Amazon", "eBay", "Onliner"];
+// let arr = [
+//     {name: Google, href = 'http://google.com'}, 
+//     {name: Amazon, href = 'http://amazon.com'}, 
+//     {name: eBay, href = 'http://ebay.com'}, 
+//     {name: Onliner, href = 'http://onliner.by'}];
+
+let arrHref = [href = 'http://google.com', href = 'http://amazon.com', href = 'http://ebay.com', href = 'http://onliner.by'];
+
+let cont = document.querySelector ('.header');
+let ul = document.createElement('ul');
+    ul.className = "header_item";
+for (i = 0; i <= arr.length - 1; i++) {
+    var li = document.createElement('li');     
+    li.innerHTML = arr[i];      
+    //li.setAttribute('style', 'display: block;');   
+    li.className = "header_item_li";
+        ul.appendChild(li);     
+};
+cont.appendChild(ul); 
+
+
+
+
+
+
+let p1 = document.createElement("p1");
+    p1.className = "about_company body_text";
+    p1.innerHTML = "О компании <br>";
+    document.body.append(p1);
+
+//  let a = document.getElementsByClassName('header_item_li');
+//  for (let index = 0; index < array.length; index++) {
+//      const element = array[index];
+//      array[0].replaceWith = <a href = '.about_company'>О компании</a>;
+//      array[1].innerHTML = <a href = '.catalogue'>О компании</a>;
+//      array[2].innerHTML = <a href = '.news'>О компании</a>;
+//      array[3].innerHTML = <a href = '.contacts'>О компании</a>;
+     
+
+//  }
+    
+ 
+//  console.log(a[i]);
+// header_item_li.item[0].innerHTML = <a href = '.about_company'>О компании</a>;    
+
+    
+let p2 = document.createElement("p2");
+    p2.className = "catalogue body_text";
+    p2.innerHTML = "Каталог <br>";
+    document.body.append(p2);
+    
+
+let p3 = document.createElement("p3");
+    p3.className = "news body_text";
+    p3.innerHTML = "Новости <br>";
+    document.body.append(p3);
+
+let p4 = document.createElement("p4");
+    p4.className = "contacts body_text";
+    p4.innerHTML = "Контакты <br>";
+    document.body.append(p4);
+    
+// let a1 = document.createElement('a1');
+//     a1.document.querySelector ('.about_company');
+//     a1.innerHTML = "href='.about_company'";
+//     li.appendChild(a1);
