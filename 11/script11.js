0ac0d8e804c51fed10b0afb787141bc9c44ecd53
ZhukAndrei <93341242+ZhukAndrei@users.noreply.github.com>
2021-12-10 13:13:18 +0300

    Added #11. Step 1. Begin.

---
+++ 11/script11.js
@@ -0,0 +1,7 @@
+//Создать функцию, для создания элементов формы, результат функции вставить в форму
+//const formArray = [{ type: text, name: "text" }, { type: password, name: "passwords" }];
+
+const form = document.createElement("form");
+document.body.appendChild(form);
+
+
