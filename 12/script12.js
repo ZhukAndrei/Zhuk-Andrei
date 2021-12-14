753c722bdfc8021a5bc05a56081246e4b9cf6284
ZhukAndrei <93341242+ZhukAndrei@users.noreply.github.com>
2021-12-14 15:56:11 +0300

    Added #12. Step 1. Begin.

---
+++ 12/script12.js
@@ -0,0 +1,13 @@
+const innerData = {
+    "id":4022,
+    "uid":"e36e674c-05e2-4354-bdc0-56c0aac8e044",
+    "plan":"Free Trial",
+    "status":"Active",
+    "payment_method":"Cash",
+    "subscription_term":"Biennal",
+    "payment_term":"Monthly"
+};
+const form = document.createElement("form")
+form.classList.add("form");
+document.body.appendChild(form);
+console.log(body);
