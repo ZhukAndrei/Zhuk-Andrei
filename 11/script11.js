361a1787d82027d5c4bcbaf06fcbff88819ae328
ZhukAndrei <93341242+ZhukAndrei@users.noreply.github.com>
2021-12-14 15:15:30 +0300

    Added #11. Step 2. Final.

--- 11/script11.js
+++ 11/script11.js
@@ -1,7 +1,27 @@
 //Создать функцию, для создания элементов формы, результат функции вставить в форму
-//const formArray = [{ type: text, name: "text" }, { type: password, name: "passwords" }];
-
+const formArray = [
+  {type: "Text1", name: "Name"},
+  {type: "text2", name: "Surname"},
+  {type: "text3", name: "Passwords"}
+];
 const form = document.createElement("form");
+form.classList.add("form");
+
 document.body.appendChild(form);
 
+const userCard = (type, name) => {
+    return `
+<ul class = "userCardForm">
+<li>${type}</li>
+<li>${name}</li>
+</ul>`
+};
+const formHTML = formArray.map((user) => {
+  return userCard(user.type, user.name)
+});
 
+form.innerHTML = formHTML;
+
+
+
+console.log(form);
