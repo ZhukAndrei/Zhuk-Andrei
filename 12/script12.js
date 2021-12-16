e47fa26f96b4af5d23b43ab5c033e38a35a2aa0d
ZhukAndrei <93341242+ZhukAndrei@users.noreply.github.com>
2021-12-16 11:44:32 +0300

    Added #12. Step 2. Received data from random.

--- 12/script12.js
+++ 12/script12.js
@@ -1,13 +1,18 @@
-const innerData = {
-    "id":4022,
-    "uid":"e36e674c-05e2-4354-bdc0-56c0aac8e044",
-    "plan":"Free Trial",
-    "status":"Active",
-    "payment_method":"Cash",
-    "subscription_term":"Biennal",
-    "payment_term":"Monthly"
-};
+const innerUrl = "https://random-data-api.com/api/business_credit_card/random_card";
+//===
+// {
+//     "id":4944,
+//     "uid":"ef52516b-336e-4d7b-891f-493b008246ad",
+//     "credit_card_number":"1234-2121-1221-1211",
+//     "credit_card_expiry_date":"2023-12-16",
+//     "credit_card_type":"american_express"
+// }
+//=======
 const form = document.createElement("form")
 form.classList.add("form");
 document.body.appendChild(form);
-console.log(body);
+console.log(document.body);
+const listPromise = fetch(innerUrl);
+listPromise
+.then(datajson =>datajson.json())
+.then(creditCardData => console.log(creditCardData));
