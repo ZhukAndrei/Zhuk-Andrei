e12ffa561b83c3b0f8336f5065e87cab9498e6e5
ZhukAndrei <93341242+ZhukAndrei@users.noreply.github.com>
2021-12-18 22:01:14 +0300

    Added #13. Step 1. Begin.

---
+++ 13/script13.js
@@ -0,0 +1,6 @@
+const innerUrlCompany =
+  "https://random-data-api.com/api/company/random_company";
+const innerUrlUser = 
+  "https://random-data-api.com/api/users/random_user";
+const innerUrlCompany =
+  "https://random-data-api.com/api/phone_number/random_phone_number";
