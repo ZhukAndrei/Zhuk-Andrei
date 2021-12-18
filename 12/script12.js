6b15238284bb510d3eef5e84de1d6db2d84e4659
ZhukAndrei <93341242+ZhukAndrei@users.noreply.github.com>
2021-12-18 20:41:34 +0300

    Added #12. Step 4. Working variant.

--- 12/script12.js
+++ 12/script12.js
@@ -12,6 +12,13 @@ const innerUrl =
 const form = document.createElement("form");
 form.classList.add("form");
 document.body.appendChild(form);
+
+let ul = document.createElement("ul");
+    ul.className = "form_item";
+
+// let li = document.createElement("li");
+//     li.className = "form_item_li";
+
 console.log(document.body);
 const listPromise = fetch(innerUrl);
 const objPromise = listPromise
@@ -19,23 +26,60 @@ const objPromise = listPromise
   .then((creditCardData) => {
     console.log(creditCardData);
 
-    for (let keys of Object.keys(creditCardData)) {
-      let arrKeys = keys;
-      console.log(arrKeys);
-    }
-    for (let value of Object.values(creditCardData)) {
-      let arrNames = value;
-      console.log(arrNames);
+    let arrLength = Array.from(Object.entries(creditCardData)).length;
+    console.log("arr lenght: " + arrLength);
+
+    arrKeys = Object.keys(creditCardData);
+    console.log("Keys:", arrKeys);
+
+    arrNames = Object.values(creditCardData);
+    console.log("Values:", arrNames);
+
+    arrKeysNames = Object.entries(creditCardData);
+    console.log("Key & Names:", arrKeysNames);
+
+    for (i = 0; i <= arrKeysNames.length - 1; i++) {
+      let p = document.createElement("p");
+      //let id = document.createElement("id");
+      p.innerText = (`${arrKeys[i]}` + ": " + `\n ${arrNames[i]}  \n \n`);
+      p.id = arrKeys[i];
+      //li.className = "header_item_li";
+
+      ul.appendChild(p);
+      //ul.appendChild(li);
     }
 
-    //    let ul = document.createElement("ul");
-    //    ul.className = "form_item";
-    //    let li = document.createElement("li");
-    //   a.innerText = arr[i];
-    //   a.href = arrHref[i];
-    //   li.className = "form_item_li";
-    //   li.appendChild(a);
-    //   ul.appendChild(li);
 
-    //console.log(creditCardData)
+    //let newArr =[];
+
+//     Object.keys(creditCardData).forEach((element) => {
+// if (Object.keys(creditCardData) !=="") {
+//   Object.keys(creditCardData).element = element;
+//   newArr.push(Object.keys(creditCardData)[element]);
+//         console.log(newArr);
+// }
+//       })
+    
+    
+    // for (let keys of ) {
+    //   var arrKeys = `${keys}`;
+    //   console.log("Keys:", arrKeys);
+    // }
+
+    // for (let value of Object.values(creditCardData)) {
+    //   var arrNames = `${value}`;
+    //   console.log("Values:", arrNames);
+    // }
+
+    // for (let value of Object.entries(creditCardData)) {
+    //   var arrKeyNames = `${value}`;
+    //   console.log("Key & Names:", arrKeyNames);
+    //}
+
+    //li.innerText = `${arrKeys}` + ":    " + `${arrNames.toUpperCase()}`;
+    //li.id = `${arrKeys}`;
+
   });
+
+form.appendChild(ul);
+//ul.appendChild(p);
